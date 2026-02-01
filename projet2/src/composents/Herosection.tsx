b014ee93689cdf5f3309200762539b290e6cfd48
unknown <ndiayeziza1999@gmail.com>
2026-02-01 21:08:23 +0000

    message

--- projet2/src/composents/Herosection.tsx
+++ projet2/src/composents/Herosection.tsx
@@ -7,8 +7,8 @@ const Herosection = () => {
       <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
         VirtualR build tools
         <span className="bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text">
-          {" "}
-          for developers
+          {" "} 
+          for <br />git  developers
         </span>
       </h1>
       <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
